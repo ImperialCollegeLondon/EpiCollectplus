9a5e7810f10512673bc9f2cdd089df18b37e1263
Chris Powell <chris.i.powell@gmail.com>
2012-06-15 16:00:58 +0100

    Edit entry and json generation fix

--- main.php
+++ main.php
@@ -1609,7 +1609,7 @@ function formHandler()
 					if($res !== true) die($res);
 					echo '[';		
 					$i = 0;			
-					while($str = preg_replace("/[\r\n]/g". ' ', $prj->tables[$frmName]->recieve(1)))
+					while($str = preg_replace("/[\r\n]/g", ' ', $prj->tables[$frmName]->recieve(1)))
 					{
 						echo ($i > 0 ? sprintf(',%s', $str) : $str);
 						$i++;
