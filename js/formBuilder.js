7737282b16cac527b4ac55b93937fc20f4b8e459
Chris Powell <chris.i.powell@gmail.com>
2012-07-30 17:53:20 +0100

    fix for issue #13 and unhiding of hidden control for key fields

--- js/formBuilder.js
+++ js/formBuilder.js
@@ -108,11 +108,11 @@ $(function()
 	$("#key").change(function(evt){
 		if(evt.target.checked)
 		{
-			$("[allow=key]").show();
+			$("[allow*=key]").show();
 		}
 		else
 		{
-			$("[allow=key]").hide();
+			$("[allow*=key]").hide();
 		}
 	});
 	
@@ -446,7 +446,11 @@ function updateForm()
 {
 	if(!currentForm) return true;
 	if(!updateSelected()) return false;	
-	if(!currentForm.key) return false;
+	if(!currentForm.key)
+	{
+		alert("The form " + currentForm.name + " needs a key defined.")
+		return false;
+	}
 	
 	var fields = {};
 	var form = currentForm;
@@ -600,7 +604,7 @@ function setSelected(jqEle)
 		
 		if(currentControl.isKey)
 		{
-			$("[allow=key]").show();
+			$("[allow*=key]").show();
 		}
 		if(jqEle.hasClass("ecplus-form-element"))
 		{
@@ -872,13 +876,15 @@ function switchToForm(name)
 
 function saveProject()
 {
+	
+	
+	if(!updateSelected()) return;
+	if(!updateForm()) return;
+	
 	var loader = new EpiCollect.LoadingOverlay();
 	loader.setMessage('Saving...');
 	loader.start();
 	window.loader = loader;
-	
-	if(!updateSelected()) return;
-	if(!updateForm()) return;
 	
 	$.ajax("./updateStructure" ,{
 		type : "POST",
