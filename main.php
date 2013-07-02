9c9fb47aba372dd004d13347d97dfaa2e8447424
Chris I Powell <chris.i.powell@gmail.com>
2013-07-02 10:10:47 +0100

    Last touches

--- main.php
+++ main.php
@@ -332,7 +332,7 @@ function applyTemplate($baseUri, $targetUri = false, $templateVars = array())
 		else
 		{
             global $PUBLIC;
-			$template = str_replace('{#loggedIn#}', '<a href="{#SITE_ROOT#}/login.php">Log in</a>' . ($PUBLIC ? ' or <a href="{#SITE_ROOT#}/register">register</a>' : ''), $template);
+			$template = str_replace('{#loggedIn#}', '<a href="{#SITE_ROOT#}/login.php">Log in</a>'/* . ($PUBLIC ? ' or <a href="{#SITE_ROOT#}/register">register</a>' : '')*/, $template);
 		}
 		// work out breadcrumbs
 		//$template = str_replace("{#breadcrumbs#}", '', $template);
