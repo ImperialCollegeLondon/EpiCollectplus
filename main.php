fe877125582c55d34f5c081a2262fe7af31431f4
Mirko Menegazzo <mirko.menegazzo@gmail.com>
2016-10-24 19:13:06 +0100

    switch to most recents project list due to memory leak

--- main.php
+++ main.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 ini_set('max_execution_time', 5000);
-ini_set('memory_limit', '512M');
+ini_set('memory_limit', '8192M');
 
 if (isset($_REQUEST['_SESSION']))
     throw new Exception('Bad client request');
@@ -965,10 +965,10 @@ function siteHome() {
     }
 
 //get most recents projects
-   // $res = $db->do_query("SELECT name FROM project Where project.isListed = 1 ORDER BY id desc LIMIT 10");
+    $res = $db->do_query("SELECT name FROM project Where project.isListed = 1 ORDER BY id desc LIMIT 10");
 
-    //get most popular projects
-    $res = $db->do_query("SELECT name, ttl, ttl24 FROM (SELECT name, count(entry.idEntry) as ttl, x.ttl as ttl24 FROM project left join entry on project.name = entry.projectName left join (select count(idEntry) as ttl, projectName from entry where created > ((UNIX_TIMESTAMP() - 86400)*1000) group by projectName) x on project.name = x.projectName Where project.isListed = 1 group by project.name) a order by ttl desc LIMIT 5");
+//    //get most popular projects
+//    $res = $db->do_query("SELECT name, ttl, ttl24 FROM (SELECT name, count(entry.idEntry) as ttl, x.ttl as ttl24 FROM project left join entry on project.name = entry.projectName left join (select count(idEntry) as ttl, projectName from entry where created > ((UNIX_TIMESTAMP() - 86400)*1000) group by projectName) x on project.name = x.projectName Where project.isListed = 1 group by project.name) a order by ttl desc LIMIT 5");
     if ($res !== true) {
 
         //$vals["projects"] = "<p class=\"error\">Database is not set up correctly, go to the <a href=\"test\">test page</a> to establish the problem.</p>";
